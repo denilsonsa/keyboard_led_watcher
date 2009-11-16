07fb541e51e6760507233a1f3834f3d3686bc8c2
Denilson Sá <denilsonsa@gmail.com>
2009-11-16 19:22:40 -0200

    Removed the non-working code related to multimedia keys. Removed some other bits.
    This project should be a good LED display, and just that, nothing else. Thus, it's wrong (at least for now) to add multimedia keys here.

--- xkbwatch.h
+++ xkbwatch.h
@@ -4,9 +4,6 @@
 #ifndef xkbwatch_included
 #define xkbwatch_included
 
-//#include <kapp.h>
-//#include <dcopclient.h>
-
 extern "C" {
 #include <X11/X.h>
 #include <X11/Xlib.h>
@@ -18,20 +15,6 @@ extern "C" {
 #define NUM_LOCK_MASK    0x00000002
 #define CAPS_LOCK_MASK   0x00000001
 #define SCROLL_LOCK_MASK 0x00000004
-
-/* special keys from logitech keyboard */
-#define KLEDS_KEY_STANDBY 223
-#define KLEDS_KEY_MUTE    160
-#define KLEDS_KEY_LOWER   174
-#define KLEDS_KEY_RAISE   176
-#define KLEDS_KEY_PLAY    162
-#define KLEDS_KEY_STOP    164
-#define KLEDS_KEY_PREV    144
-#define KLEDS_KEY_NEXT    153
-#define KLEDS_KEY_MAIL    236
-#define KLEDS_KEY_SEARCH  229
-#define KLEDS_KEY_START   230
-#define KLEDS_KEY_HOME    178
 
 /* class xkbwatch the indicator watcher class */
 
@@ -56,8 +39,5 @@ private:
 
 protected:
   void indicators_changed(int ledstate);
-  void send_pid();
-  void register_key(int tcode, KeySym newKS);
-  void specialkey_pressed(int keycode);
 };
 #endif
